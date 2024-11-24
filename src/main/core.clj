7be63d026a9d958c7c319a03f254fd7e6ed21db4
arvchahal <arnavchahal14@gmail.com>
2024-11-23 22:30:41 -0500

    Revert "yes"
    This reverts commit 4391c444c186bda3108a96e9525d7c6ec740791f.

--- src/main/core.clj
+++
@@ -1,8 +0,0 @@
-(ns main.core
-  (:require [clojure.java.shell :refer [sh]]))
-(defn get-cpu []
-  (:out (sh "top" "-l" "1" "-o" "cpu")))
-(defn -main [& args]
-  (println (get-cpu))
-  (println ()))
-
