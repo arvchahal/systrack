7124207c7a24520e988c77f3fb62857c13a42898
arvchahal <arnavchahal14@gmail.com>
2024-11-29 13:20:31 -0500

    ding

--- src/main/core.clj
+++ src/main/core.clj
@@ -1,8 +1,30 @@
 (ns main.core
-  (:require [clojure.java.shell :refer [sh]]))
-(defn get-cpu []
-  (:out (sh "top" "-l" "1" "-o" "cpu")))
-(defn -main [& args]
-  (println (get-cpu))
-  (println ()))
+  (:require
+   [util.helpers :refer [string-to-map-cpu-usage string-to-map-cpu-load]]
+   [clojure.java.shell :refer [sh]]
+   [clojure.string :as str]))
 
+;function for cpu usage
+(defn get-cpu-usage []
+  ;; Run the shell command and capture the output
+  (let [result (sh "sh" "-c" "top -l 1 | grep 'CPU usage'")]
+    (:out result))) 
+
+
+;function for cpu load
+(defn get-cpu-load []
+  ;;gets past 5, 10,15 minute loads w/o any string manipulation done
+  (let [result (sh "sh" "-c" "top -l 1 | grep 'Load Avg'")] 
+    (:out result)))
+
+(defn -main [& args]
+  (let [cpu-info (get-cpu-usage) cpu-load (get-cpu-load)]
+    (if (str/blank? cpu-info)
+      (println "No CPU data available.") 
+      (if (str/blank? cpu-load)
+        (println "no cpu load info")
+      
+        (let [cpu_map (string-to-map-cpu-usage cpu-info) cpu_load (string-to-map-cpu-load cpu-load) ]
+          
+        (println "Command Output:") ;;This is the converted cpu usage to a map
+        (println (keys cpu_map) (vals cpu_load)))))))
--- src/util/helpers.clj
+++ src/util/helpers.clj
@@ -1,1 +1,36 @@
-(ns util.helpers)
+(ns util.helpers
+  (:require [clojure.string :as str]))
+;;
+;;
+;;  This file contains some rather annoying string to map conversions of the 
+;;  command line outputs.
+;;
+;;
+
+
+;; for the cpu usage command that gets what entity is using what % of the cpu
+;; fields user, system, idle.
+;; appears in a weird order output is like
+;; 12.4% user, 22% system,... not like key-value so that is why the code is a bit weird
+(defn string-to-map-cpu-usage [cpu-string]
+  (let [stripped (str/replace cpu-string #"CPU usage: " "")
+        parts (str/split stripped #", ")
+        parsed (map (fn [part]
+                      (let [[value key] (str/split part #" ")]
+                        [(keyword key) (Double/parseDouble (str/replace value #"%" ""))]))
+                    parts)]
+    (into {} parsed)))
+
+;; for the cpu load command
+;; a bit easier as output is only numbers like 1.23,2.5,...
+;; still annoying
+;; 
+(defn string-to-map-cpu-load [cpu-load]
+  (let [time [1 5 15] ;;time periods of the command outputs
+        stripped (str/replace cpu-load #"Load Avg: " "")
+        parts (str/split stripped #", ")
+        parsed (map (fn [t value]
+                      [(keyword (str t "min")) (Double/parseDouble value)])
+                    time
+                    parts)]
+    (into {} parsed)))
